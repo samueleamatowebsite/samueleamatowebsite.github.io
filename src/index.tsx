73c4fb049f8845256e9e73ee0fc0a8fe3589a1bd
rdwei <samuele.amato.2006@proton.me>
2024-12-23 22:02:01 +0100

    -

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portfolio from './components/Portfolio';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
